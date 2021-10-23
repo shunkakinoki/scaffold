90e259d1133d1cb522169a7733fcc532239b39e5
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-23 13:10:34 -0400

    chore: update

--- packages/eth/tasks/deploy/greeter.ts
+++ packages/eth/tasks/deploy/greeter.ts
@@ -1,15 +1,11 @@
 import { task } from "hardhat/config";
 import type { TaskArguments } from "hardhat/types";
 
-import type { Greeter } from "@/typechain/Greeter";
-
 task("deploy:Greeter")
   .addParam("greeting", "Say hello, be nice")
   .setAction(async (taskArguments: TaskArguments, { ethers }) => {
     const greeterFactory = await ethers.getContractFactory("Greeter");
-    const greeter: Greeter = <Greeter>(
-      await greeterFactory.deploy(taskArguments.greeting)
-    );
+    const greeter = await greeterFactory.deploy(taskArguments.greeting);
     await greeter.deployed();
     console.log("Greeter deployed to: ", greeter.address);
   });
