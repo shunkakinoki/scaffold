3d6a5b9c0e5a885217c66915223f42481ff01380
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-23 16:11:07 -0400

    chore: update

--- packages/eth/types/index.ts
+++ packages/eth/types/index.ts
@@ -1,11 +1,12 @@
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import type { Fixture } from "ethereum-waffle";
 
-import type { Greeter } from "typechain/Greeter";
+import type { Greeter } from "@/typechain/Greeter";
 
 declare module "mocha" {
   export interface Context {
     greeter: Greeter;
+    // eslint-disable-next-line no-unused-vars
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
     signers: Signers;
   }
